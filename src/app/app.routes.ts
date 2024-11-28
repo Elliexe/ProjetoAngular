101f9ca7ed753d0694443bda069666128644e069
Gustavo <gugagui02092004@gmail.com>
2024-11-27 23:17:56 -0300

    mesclagem angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+    
+
+{
+    path:'header', component:HeaderComponent   
+},
+{
+    path:'login', component:LoginComponent  
+}];
